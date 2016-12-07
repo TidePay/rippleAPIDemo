08db4fb2004f844b0e9d572d13b9b69ce66e97d9
alex.choi <alex.choi@isuntv-hk.com>
2016-12-07 11:06:23 +0800

    fixed path problem which leads index.html run without node.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'scripts')));
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
